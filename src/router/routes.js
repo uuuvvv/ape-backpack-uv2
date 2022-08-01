c6492b3b4531bba79641df2b3b31bbb70977dad5
it-zhang <jyzhl11@163.com>
2022-08-01 10:46:41 +0800

    路由mate

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,10 +1,16 @@
+// 主页
 const home = () => import("@/views/home.vue");
-
+// 登录页
 const loginPage = () => import("@/views/loginPage.vue");
+// 主页内容
 const homePage = () => import("@/views/homePage.vue");
+// 网络书签
 const webTagsPage = () => import("@/views/webTagsPage.vue");
+// 八股
 const eightPartPage = () => import("@/views/eightPartPage.vue");
+// 消灭bug
 const killBugPage = () => import("@/views/killBugPage.vue");
+// 挑花猿
 const peachPage = () => import("@/views/peachPage.vue");
 
 export default [
@@ -18,35 +24,47 @@ export default [
     path: "/home",
     name: "home",
     component: home,
-    meta: {
-      title: "首页",
-    },
     redirect: "/home/homePage",
     children: [
       {
         path: "/home/homePage",
         name: "homePage",
         component: homePage,
+        meta: {
+          title: "首页",
+        },
       },
       {
         path: "/home/webTagsPage",
         name: "webTagsPage",
         component: webTagsPage,
+        meta: {
+          title: "网络书签",
+        },
       },
       {
         path: "/home/eightPartPage",
         name: "eightPartPage",
         component: eightPartPage,
+        meta: {
+          title: "猿八股",
+        },
       },
       {
         path: "/home/killBugPage",
         name: "killBugPage",
         component: killBugPage,
+        meta: {
+          title: "消灭BUG",
+        },
       },
       {
         path: "/home/peachPage",
         name: "peachPage",
         component: peachPage,
+        meta: {
+          title: "桃花源",
+        },
       },
     ],
   },
@@ -55,5 +73,8 @@ export default [
     path: "/login",
     name: "login",
     component: loginPage,
+    meta: {
+      title: "登录",
+    },
   },
 ];
